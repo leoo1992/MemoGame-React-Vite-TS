412c154d297b86916c6e315a90c718668438ff09
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-03-19 18:11:30 -0300

    bugs fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './templates/App'
-import './styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './templates/App';
+import './styles/index.css';
+
+window.console.warn = () => {};
+window.console.info = () => {};
+window.console.error = () => {};
+window.console.log = () => {};
+window.console.clear();
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
+
+
+
+
--- src/templates/App/index.tsx
+++ src/templates/App/index.tsx
@@ -1,14 +1,45 @@
+import { useEffect } from "react";
 import Grids from "../../components/Grid";
 import { cards } from "../../data/cards";
 
 export default function App() {
-  // const handleClick = (id: string) => {
-  //   console.log(id);
-  // };
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key !== "F12") {
+        event.preventDefault();
+      }
+    }
+
+    function handleWheel(event: WheelEvent) {
+      event.preventDefault();
+    }
+
+    function handleContextMenu(event: MouseEvent) {
+      event.preventDefault();
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("wheel", handleWheel, { passive: false });
+    window.addEventListener("contextmenu", handleContextMenu);
+
+    console.warn = () => {};
+    console.info = () => {};
+    console.error = () => {};
+    console.log = () => {};
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("wheel", handleWheel);
+      window.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-col w-screen h-screen m-0 p-0
-    bg-gradient-to-br from-red-100 via-sky-50 to-blue-100">
-      <Grids cards={cards}/>
+    <div
+      className="flex flex-col w-screen h-screen m-0 p-0
+    bg-gradient-to-br from-red-100 via-sky-50 to-blue-100"
+    >
+      <Grids cards={cards} />
     </div>
   );
 }
